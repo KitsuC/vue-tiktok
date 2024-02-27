93f6c5a32dfae3b6a1f4b853e1ce74bee89ac700
KitsuC <83906764+KitsuC@users.noreply.github.com>
2024-02-27 17:31:46 +0700

    nev

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    pages:true,
+    css: ['~/assets/css/main.css'],
+    modules: [
+        'nuxt-icon',
+        '@pinia/nuxt',
+        '@pinia-plugin-persistedstate/nuxt',
+    ],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
 })
